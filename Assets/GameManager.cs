210e3921626751c6202a2d93025416b4f0053598
Your Name <you@example.com>
2024-10-07 15:28:30 +0900

    add  ゲームリスタートボタンの追加

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private GameObject text;
+    private GameObject restartButton;
     // Start is called before the first frame update
     void Start()
     {
         text = GameObject.Find("Text");
+        restartButton = GameObject.Find("RestartButton");
+        restartButton.SetActive(false);
     }
 
     // Update is called once per frame
@@ -20,5 +24,10 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         text.GetComponent<Text>().text = "Game Over";
+        restartButton.SetActive(true);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
