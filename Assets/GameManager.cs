d940ae394d5f2cd378c40ea1861083df3e4cc6bb
Your Name <you@example.com>
2024-10-07 15:55:21 +0900

    add げーむくりあを追加

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,18 +8,24 @@ public class GameManager : MonoBehaviour
 {
     private GameObject text;
     private GameObject restartButton;
+    private GameObject[] blocks;
     // Start is called before the first frame update
     void Start()
     {
         text = GameObject.Find("Text");
         restartButton = GameObject.Find("RestartButton");
         restartButton.SetActive(false);
+        blocks = GameObject.FindGameObjectsWithTag("Block");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        blocks = GameObject.FindGameObjectsWithTag("Block");
+        if (blocks.Length==0)
+        {
+            GameClear();
+        }
     }
     public void GameOver()
     {
@@ -29,5 +35,9 @@ public class GameManager : MonoBehaviour
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void GameClear()
+    {
+        text.GetComponent<Text>().text = "Game Clear";
     }
 }
