c29a7836fc377420a0ec20d289836a0969f33e50
Your Name <you@example.com>
2024-10-21 14:33:04 +0900

    add new stage

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,14 +6,20 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public string nextSceneName;
     private GameObject text;
     private GameObject restartButton;
+    private GameObject nextButton;
     // Start is called before the first frame update
     void Start()
     {
         text = GameObject.Find("Text");
         restartButton = GameObject.Find("RestartButton");
+        nextButton = GameObject.Find("NextButton");
         restartButton.SetActive(false);
+        nextButton.SetActive(false);
+
+        blocks = GameObject.FindGameObjectWithTag("Block");
     }
 
     // Update is called once per frame
@@ -29,5 +35,20 @@ public class GameManager : MonoBehaviour
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void GameClear()
+    {
+        text.GetComponent<Text>().text="Game Clear";
+        nextButton.SetActive(true);
+    }
+    public void NextStage()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+    private IEnumerator ShowStageName()
+    {
+        text.GetComponent<Text>().text = SceneManager.GetActiveScene().name;
+        yield return new WaitForSeconds(2.0f);
+        text.GetComponent<Text>().text = "";
     }
 }
